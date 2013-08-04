42b7619cfcfc466344b9e29677dceaafc77edf5f
Derek Willis <dwillis@gmail.com>
2013-08-04 06:43:37 -0400

    added doc string

--- openelex/us/md/fetch.py
+++ openelex/us/md/fetch.py
@@ -2,10 +2,9 @@ from openelex.base.fetch import BaseScraper
 from openelex.us.md.geo import jurisdictions
 import urlparse
 
-# output is a file saved locally inside dir (git-ignored cache dir)
-# make base_url and path and recreate directory structure
-
 """
+Retrieves CSV result files for a given year from Maryland State Board of Elections and caches them locally.
+
 File name for general election precinct-level files is county_name_by_precinct_year_general.csv
 File name for general election state legislative district-level files is State_Legislative_Districts_year_general.csv
 File name for general election county-level files is County_Name_party_year_general.csv
@@ -14,6 +13,11 @@ File name for primary election precinct-level files is County_Name_by_Precinct_p
 File name for primary election state legislative district-level files is State_Legislative_Districts_party_year_primary.csv
 File name for primary election county-level files is County_Name_party_year_primary.csv
 
+Usage:
+
+from openelex.us.md import fetch
+f = fetch.FetchResults()
+f.run(2012)
 """
 
 class FetchResults(BaseScraper):
