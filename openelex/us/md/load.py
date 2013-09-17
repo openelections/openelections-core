3f890a1c37d245af145896655e7c929db7a20d88
Derek Willis <dwillis@gmail.com>
2013-09-17 18:39:44 -0400

    handle rows with no votes

--- openelex/us/md/load.py
+++ openelex/us/md/load.py
@@ -6,6 +6,15 @@ from openelex.models import Candidate, Result, Contest
 import json
 import csv
 
+"""
+Usage:
+
+from openelex.us.md import load
+l = load.LoadResults()
+l.run(2012)
+"""
+
+
 class LoadResults(BaseLoader):
     
     def run(self, year):
@@ -33,7 +42,6 @@ class LoadResults(BaseLoader):
                 reader = csv.DictReader(csvfile)
                 if 'state_legislative' in f['generated_name']:
                     reporting_level = 'state_legislative'
-                    # fix ' LEGS 26'
                     districts = [j for j in reader.fieldnames if j not in ['Vote Type (FOR=1,AGAINST=2)', 'County', 'Candidate Name', 'Party', 'Office Name', 'Office District', 'Winner', 'Write-In?']]
                 elif 'county' in f['generated_name']:
                     reporting_level = 'county'
@@ -43,6 +51,8 @@ class LoadResults(BaseLoader):
                     reporting_level = 'state'
                     jurisdiction = "ocd-division/country:us/state:md"
                 for row in reader:
+                    if not row['Office Name'] in ['President - Vice Pres', 'U.S. Senator           ', 'U.S. Congress']:
+                        pass
                     # parse candidate - we get full names
                     name = self.parse_name(row['Candidate Name'])
                     # if office is president, then skip state in lookup, otherwise use
@@ -54,16 +64,27 @@ class LoadResults(BaseLoader):
                         write_in = True
                     else:
                         write_in = False
+                    if row['Winner'] == 'Y':
+                        winner = True
+                    else:
+                        winner = False
                     if reporting_level == 'state_legislative':
                         for district in districts:
+                            print district
+                            if row[district].strip() == '':
+                                votes = 0
+                            else:
+                                votes = row[district]
                             ocd = "ocd-division/country:us/state:md/sldl:%s" % district.strip().lower()
-                            result = Result(ocd_id=ocd, jurisdiction=district.strip(), raw_office=row['Office Name'], reporting_level=reporting_level, candidate=candidate, party=row['Party'], write_in=write_in, total_votes=row[district]).save()
+                            result = Result(ocd_id=ocd, jurisdiction=district.strip(), raw_office=row['Office Name'].strip(), reporting_level=reporting_level, candidate=candidate, party=row['Party'], write_in=write_in, total_votes=votes, winner=winner).save()
                     elif reporting_level == 'county':
                         jurisdiction = f['name']
                         total_votes = row['Total Votes']
+                        vote_breakdowns = { 'election_night_total': row['Election Night Votes'], 'absentee_total': row['Absentees Votes'], 'provisional_total': row['Provisional Votes'], 'second_absentee_total': row['2nd Absentees Votes']}
                     elif reporting_level == 'precinct':
                         jurisdiction = str(row['Election District'])+"-"+str(row['Election Precinct'])
                         total_votes = row['Election Night Votes']
+                        vote_breakdowns = { 'election_night_total': row['Election Night Votes']}
                     if not reporting_level == 'state_legislative':
-                        result = Result(ocd_id=f['ocd_id'], jurisdiction=jurisdiction, raw_office=row['Office Name'], reporting_level=reporting_level, candidate=candidate, party=row['Party'], write_in=write_in, total_votes=total_votes).save()
+                        result = Result(ocd_id=f['ocd_id'], jurisdiction=jurisdiction, raw_office=row['Office Name'], reporting_level=reporting_level, candidate=candidate, party=row['Party'], write_in=write_in, total_votes=total_votes, vote_breakdowns=vote_breakdowns).save()
         
