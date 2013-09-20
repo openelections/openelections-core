cda8cd871c09dbd317b98b7bfa20bea7f13a9d09
Derek Willis <dwillis@gmail.com>
2013-09-20 17:07:42 -0400

    basic loader for md, minus Contest

--- openelex/us/md/load.py
+++ openelex/us/md/load.py
@@ -4,7 +4,7 @@ from openelex.us.md import fetch
 from mongoengine import *
 from openelex.models import Candidate, Result, Contest
 import json
-import csv
+import unicodecsv
 
 """
 Usage:
@@ -12,6 +12,13 @@ Usage:
 from openelex.us.md import load
 l = load.LoadResults()
 l.run(2012)
+
+Maybe add flags to load only certain reporting levels?
+
+re-run filenames loader to write election_dates
+pass files as hash with keys of election_dates and lists of interior files, with jurisdiction names
+WHY IS THERE A 20120403__md__general__state_legislative.csv file?
+delete this: 20121106__md__general____precinct.csv
 """
 
 
@@ -36,20 +43,18 @@ class LoadResults(BaseLoader):
         # logging?
     
     def process_files(self, files):
-        connect('openelex_md')
+        connect('openelex_md_tester')
         for f in files:
+            print f['generated_name']
             with open(join(self.cache_dir, f['generated_name'])) as csvfile:
-                reader = csv.DictReader(csvfile)
+                reader = unicodecsv.DictReader(csvfile, encoding='latin-1')
                 if 'state_legislative' in f['generated_name']:
                     reporting_level = 'state_legislative'
                     districts = [j for j in reader.fieldnames if j not in ['Vote Type (FOR=1,AGAINST=2)', 'County', 'Candidate Name', 'Party', 'Office Name', 'Office District', 'Winner', 'Write-In?']]
-                elif 'county' in f['generated_name']:
-                    reporting_level = 'county'
                 elif 'precinct' in f['generated_name']:
                     reporting_level = 'precinct'
                 else:
-                    reporting_level = 'state'
-                    jurisdiction = "ocd-division/country:us/state:md"
+                    reporting_level = 'county'
                 for row in reader:
                     if not row['Office Name'].strip() in ['President - Vice Pres', 'U.S. Senator', 'U.S. Congress', 'Governor / Lt. Governor', 'Comptroller', 'Attorney General', 'State Senator', 'House of Delegates']:
                         pass
@@ -61,33 +66,52 @@ class LoadResults(BaseLoader):
                         candidate = Candidate(state='US', given_name=name.first, additional_name=name.middle, family_name=name.last, suffix=name.suffix, name=name.full_name)
                     else:
                         candidate = Candidate(state=self.state.upper(), given_name=name.first, additional_name=name.middle, family_name=name.last, suffix=name.suffix, name=name.full_name)
-                    if row['Write-In?'] == 'Y':
-                        write_in = True
-                    else:
-                        write_in = False
+                    # sometimes write-in field not present
+                    try:
+                        if row['Write-In?'] == 'Y':
+                            write_in = True
+                        else:
+                            write_in = False
+                    except KeyError:
+                        write_in = None
                     if row['Winner'] == 'Y':
                         winner = True
                     else:
                         winner = False
                     if reporting_level == 'state_legislative':
-                        for district in districts:
-                            print district
-                            if row[district].strip() == '':
-                                votes = 0
-                            else:
-                                votes = row[district]
-                            if row['Party'] not in candidate.raw_parties:
-                                candidate.raw_parties.append(row['Party'])
-                            ocd = "ocd-division/country:us/state:md/sldl:%s" % district.strip().split(' ')[1]
-                            result = Result(ocd_id=ocd, jurisdiction=district.strip(), raw_office=row['Office Name'].strip(), reporting_level=reporting_level, candidate=candidate, party=row['Party'], write_in=write_in, total_votes=votes, winner=winner).save()
+                        self.process_state_legislative(row, districts, candidate, reporting_level, write_in, winner)
                     elif reporting_level == 'county':
-                        jurisdiction = f['name']
-                        total_votes = row['Total Votes']
-                        vote_breakdowns = { 'election_night_total': row['Election Night Votes'], 'absentee_total': row['Absentees Votes'], 'provisional_total': row['Provisional Votes'], 'second_absentee_total': row['2nd Absentees Votes']}
+                        self.process_county(row, f, candidate, reporting_level, write_in, winner)
                     elif reporting_level == 'precinct':
-                        jurisdiction = str(row['Election District'])+"-"+str(row['Election Precinct'])
-                        total_votes = row['Election Night Votes']
-                        vote_breakdowns = { 'election_night_total': row['Election Night Votes']}
-                    if not reporting_level == 'state_legislative':
-                        result = Result(ocd_id=f['ocd_id'], jurisdiction=jurisdiction, raw_office=row['Office Name'], reporting_level=reporting_level, candidate=candidate, party=row['Party'], write_in=write_in, total_votes=total_votes, vote_breakdowns=vote_breakdowns).save()
+                        self.process_precinct(row, f, candidate, reporting_level, write_in, winner)
+    
+    def process_state_legislative(self, row, districts, candidate, reporting_level, write_in, winner):
+        for district in districts:
+            if row[district].strip() == '':
+                total_votes = 0
+            else:
+                total_votes = row[district]
+            if row['Party'] not in candidate.raw_parties:
+                candidate.raw_parties.append(row['Party'])
+            ocd = "ocd-division/country:us/state:md/sldl:%s" % district.strip().split(' ')[1]
+            result = Result(ocd_id=ocd, jurisdiction=district.strip(), raw_office=row['Office Name'].strip(), reporting_level=reporting_level, candidate=candidate, party=row['Party'], write_in=write_in, total_votes=total_votes, winner=winner).save()
+    
+    def process_county(self, f, row, candidate, reporting_level, write_in, winner):
+        try:
+            jurisdiction = f['name']
+        except:  # fix this!
+            jurisdiction = None
+        try:
+            total_votes = row['Total Votes']
+        except:
+            print row
+        vote_breakdowns = { 'election_night_total': row['Election Night Votes'], 'absentee_total': row['Absentees Votes'], 'provisional_total': row['Provisional Votes'], 'second_absentee_total': row['2nd Absentees Votes']}
+        result = Result(ocd_id=f['ocd_id'], jurisdiction=jurisdiction, raw_office=row['Office Name'], reporting_level=reporting_level, candidate=candidate, party=row['Party'], write_in=write_in, total_votes=total_votes, vote_breakdowns=vote_breakdowns).save()
         
+    def process_precinct(self, f, row, candidate, reporting_level, write_in, winner):
+        jurisdiction = str(row['Election District'])+"-"+str(row['Election Precinct'])
+        total_votes = row['Election Night Votes']
+        vote_breakdowns = { 'election_night_total': row['Election Night Votes']}
+        result = Result(ocd_id=f['ocd_id'], jurisdiction=jurisdiction, raw_office=row['Office Name'], reporting_level=reporting_level, candidate=candidate, party=row['Party'], write_in=write_in, total_votes=total_votes, vote_breakdowns=vote_breakdowns).save()
+        
+                
