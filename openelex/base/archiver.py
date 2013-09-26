f359d21beef5b2aa6170b703d7888711644582ab
Derek Willis <dwillis@gmail.com>
2013-09-26 09:34:42 -0400

    added s3_path and state to base archiver class

--- openelex/base/archiver.py
+++ openelex/base/archiver.py
@@ -12,10 +12,12 @@ class BaseArchiver(object):
     """
 
     def __init__(self):
+        self.state = self.__module__.split('.')[-2]
         self.AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
         self.AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
         self.mappings_dir = os.path.join(os.path.dirname(inspect.getfile(self.__class__)), 'mappings')
         self.cache_dir = os.path.join(os.path.dirname(inspect.getfile(self.__class__)), 'cache')
+        self.s3_path = "us/states/%s/raw/" % self.state
         self.conn = boto.connect_s3()
 
     def run(self):
--- openelex/us/md/archiver.py
+++ openelex/us/md/archiver.py
@@ -18,10 +18,9 @@ class ResultsArchiver(BaseArchiver):
     def run(self, year):
         filenames = self.filename_mappings()[str(year)]
         bucket = self.conn.get_bucket('openelex-data')
-        path = "us/states/md/raw/"
         for file in filenames:
             k = Key(bucket)
-            k.key = os.path.join(path, file['generated_name'])
+            k.key = os.path.join(self.s3_path, file['generated_name'])
             k.set_contents_from_filename(os.path.join(self.cache_dir, file['generated_name']))
         
     def filename_mappings(self):
