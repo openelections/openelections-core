2a5c649108840977a78a27da4f440aaf0e7ce449
Derek Willis <dwillis@gmail.com>
2013-09-26 09:26:59 -0400

    fleshed out base archiver

--- openelex/base/archiver.py
+++ openelex/base/archiver.py
@@ -1,7 +1,7 @@
 import logging
 import os
+import inspect
 import sys
-
 import boto
 
 class BaseArchiver(object):
@@ -11,15 +11,12 @@ class BaseArchiver(object):
     are provided.
     """
 
-    def __init__(self, AWS_ACCESS_KEY_ID='', AWS_SECRET_ACCESS_KEY='')
-        if not self.AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY:
-            print "AWS credentials not provided; will not archive to S3"
-
-        else:
-            self.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
-            self.AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
-            
-        conn = boto.connect_s3()
+    def __init__(self):
+        self.AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+        self.AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+        self.mappings_dir = os.path.join(os.path.dirname(inspect.getfile(self.__class__)), 'mappings')
+        self.cache_dir = os.path.join(os.path.dirname(inspect.getfile(self.__class__)), 'cache')
+        self.conn = boto.connect_s3()
 
     def run(self):
         raise NotImplementedError()
