cc22e385dc3062aecba45ef5f03b48043899730c
Serdar Tumgoren <zstumgoren@gmail.com>
2013-10-01 23:09:29 -0700

    revert fetch task to use year or elecdate rather than datefilter

--- tasks/fetch.py
+++ tasks/fetch.py
@@ -3,17 +3,20 @@ import os
 from invoke import task
 
 from .constants import COUNTRY_DIR
-from .utils import HELP_TEXT, load_module
+from .utils import load_module
 
-
-@task(help=HELP_TEXT)
-def fetch(state, datefilter):
+@task(help={
+    'state':'Two-letter state-abbreviation, e.g. NY',
+    'year': '4-digit election year',
+    'elecdate': 'Fetch files for a specific date YYYY-MM-DD'
+})
+def fetch(state, year='', elecdate=''):
     """
     Scrape raw data files, generate standardized names, 
     and store in local file cache.
 
-    State is required. Optionally provide date filter
-    to limit files that are fetched.
+    State is required. Optionally provide 'year' 
+    or 'elecdate' to limit files that are fetched.
     """
     state_mod = load_module(state, 'fetch')
     state_dir = os.path.join(COUNTRY_DIR, state) 
