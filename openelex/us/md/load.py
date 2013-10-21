0b949c450afb5ebae61583460bf56171003910c4
Derek Willis <dwillis@gmail.com>
2013-10-21 11:12:05 -0400

    finished 2000 primary loader

--- openelex/us/md/load.py
+++ openelex/us/md/load.py
@@ -15,7 +15,7 @@ Usage:
 
 from openelex.us.md import load
 l = load.LoadResults()
-file = l.filenames['2002'][0] # just an example
+file = l.filenames['2000'][49] # just an example
 l.load(file)
 """
 
@@ -27,6 +27,8 @@ class LoadResults(BaseLoader):
         contest = self.get_contest(file)
         if contest.year == 2002:
             self.load_2002_file(file, contest)
+        elif contest.year == 2000 and contest.election_type == 'primary': # special case for 2000 primary
+            self.load_2000_primary_file(file, contest)
         else:
             self.load_non2002_file(file, contest)
         contest.updated = datetime.datetime.now()
@@ -47,6 +49,67 @@ class LoadResults(BaseLoader):
             contest.created = datetime.datetime.now()
             contest.save()
         return contest
+    
+    def load_2000_primary_file(self, file, contest):
+        with open(join(self.cache_dir, file['generated_name']), 'rU') as csvfile:
+            lines = csvfile.readlines()
+            jurisdictions = self.jurisdiction_mappings(('ocd','fips','urlname','name'))
+            for line in lines[0:377]:
+                if line.strip() == '':
+                    continue # skip blank lines
+                else: # determine if this is a row with an office
+                    cols = line.strip().split(',')
+                    if cols[0][1:29] == 'President and Vice President':
+                        office_name = 'President - Vice Pres'
+                        if 'Democratic' in cols[0]:
+                            party = 'DEM'
+                        else:
+                            party = 'REP'
+                        continue
+                    elif cols[0][1:13] == 'U.S. Senator':
+                        office_name = 'U.S. Senator'
+                        if 'Democratic' in cols[0]:
+                            party = 'DEM'
+                        else:
+                            party = 'REP'
+                        continue
+                    elif cols[0][1:27] == 'Representative in Congress':
+                        district = int(cols[0][71:73])
+                        office_name = 'U.S. Congress ' + str(district)
+                        if 'Democratic' in cols[0]:
+                            party = 'DEM'
+                        else:
+                            party = 'REP'
+                        continue
+                    # skip offices we don't want
+                    elif cols[0][1:21] == 'Judge of the Circuit':
+                        continue
+                    elif cols[0][1:31] == 'Female Delegates and Alternate':
+                        continue
+                    elif cols[0][1:29] == 'Male Delegates and Alternate':
+                        continue
+                    elif cols[0][1:28] == 'Delegates to the Republican':
+                        continue
+                    elif cols[0] == '""':
+                        candidates = [x.replace('"','').strip() for x in cols if x.replace('"','') != '']
+                        winner = [i for i, j in enumerate(candidates) if 'Winner' in j][0] # index of winning candidate
+                        candidates[winner] = candidates[winner].split(' Winner')[0]
+                        # handle name_parse and Uncommitted candidate
+                    else: # county results
+                        result = [x.replace('"','').strip() for x in cols if x != '']
+                        juris = [j for j in jurisdictions if j['name'] == result[0].strip()][0]
+                        cand_results = zip(candidates, result[1:])
+                        for cand, votes in cand_results:
+                            name = self.parse_name(cand)
+                            if office_name == 'President - Vice Pres':
+                                candidate = Candidate(state='US', given_name=name.first, additional_name=name.middle, family_name=name.last, suffix=name.suffix, name=name.full_name)
+                            else:
+                                candidate = Candidate(state=self.state.upper(), given_name=name.first, additional_name=name.middle, family_name=name.last, suffix=name.suffix, name=name.full_name)
+                            result = Result(ocd_id=juris['ocd'], jurisdiction=juris['name'], raw_office=office_name, reporting_level='county', candidate=candidate, party=party, write_in=False, total_votes=int(votes), vote_breakdowns={})
+                            contest.update(push__results=(result))
+                                            
+                    
+        
     
     def load_2002_file(self, file, contest):
         headers = ['office', 'fill', 'jurisdiction', 'last', 'middle', 'first', 'party', 'winner', 'vote_type', 'votes', 'fill2']
@@ -72,6 +135,7 @@ class LoadResults(BaseLoader):
                     name = self.parse_name(cand_name)
                     write_in=False
                     candidate = Candidate(state=self.state.upper(), given_name=name.first, additional_name=name.middle, family_name=name.last, suffix=name.suffix, name=name.full_name)
+                result = Result(ocd_id=ocd_id+"/precinct:"+str(row['Election District'])+"-"+str(row['Election Precinct']), jurisdiction=jurisdiction, raw_office=row['Office Name']+' '+row['Office District'], reporting_level=reporting_level, candidate=candidate, party=row['Party'], write_in=write_in, total_votes=total_votes, vote_breakdowns=vote_breakdowns)
                 contest.update(push__results=(self.load_county_2002(row, jurisdiction['ocd'], jurisdiction['name'], candidate, reporting_level, write_in, winner)))
     
         
