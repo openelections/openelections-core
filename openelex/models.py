4cb42ac9c552c19e71d5630af570a1daeff7dc4a
Serdar Tumgoren <zstumgoren@gmail.com>
2013-12-05 19:51:12 -0800

    add partial work on updated models

--- openelex/models.py
+++ openelex/models.py
@@ -1,5 +1,4 @@
 import datetime
-import settings
 from mongoengine import *
 from mongoengine.fields import (
     BooleanField,
@@ -10,43 +9,101 @@ from mongoengine.fields import (
     ListField,
     StringField,
 )
+# Below import is necessary to instantiate connection object
+import settings
 
-class Candidate(EmbeddedDocument):
+
+class Office(EmbeddedDocument):
+    state = StringField()
+    name = StringField()
+    district = StringField()
+
+
+class Contest(DynamicDocument):
+    created = DateTimeField()
+    updated = DateTimeField()
+    source = StringField(required=True, help_text="Name of data source for this file, preferably standardized filename from datasource.py")
+    election_id = StringField(required=True, help_text="election id, e.g. md-2012-11-06-general")
+    slug = StringField(required=True, help_text="Slugified office name, plus district and party if relevant")
+    state = StringField(required=True)
+    start_date = DateTimeField(required=True)
+    end_date = DateTimeField(required=True)
+    election_type = StringField(help_text="general, primary, etc. from OpenElex metadata")
+    result_type = StringField(required=True)
+    special = BooleanField(default=False)
+    raw_office = StringField(required=True)
+    raw_district = StringField()
+    raw_party = StringField(help_text="This should only be assigned for closed primaries, where voters must be registered in party to vote in the contest")
+
+    # FIELDS FOR TRANSFORMED/LINKED DATA
+    office = EmbeddedDocumentField(Office)
+    district = StringField()
+    party = StringField(help_text="This should only be assigned for closed primaries, where voters must be registered in party to vote in the contest")
+
+    def __unicode__(self):
+        return u'%s-%s' % (self.election_id, self.slug)
+
+
+class Candidate(DynamicDocument):
     """
     State is included because in nearly all cases, a candidate is unique to a state (presidential candidates run in multiple states,
     but hail from a single state). This would help with lookups and prevent duplicates. Identifiers is a DictField because a candidate
     may have 0 or more identifiers, including state-level IDs.
-    """
-    uuid = StringField()
-    state = StringField(required=True)
-    given_name = StringField(max_length=200)
-    additional_name = StringField(max_length=200)
-    family_name = StringField(max_length=200)
-    suffix = StringField(max_length=200)
-    name = StringField(max_length=300, required=True)
-    other_names = ListField(StringField(), default=list)
-    raw_parties = ListField(StringField(), default=list)
-    parties = ListField(StringField(), default=list) # normalized? abbreviations?
-    identifiers = DictField()
-    
-    """
+
     parties = ['Democratic', 'Republican'] 
-    
+
     identifiers = {
         'bioguide' : <bioguide_id>,
         'fec' : [<fecid_1>, <fecid_2>, ...],
         'votesmart' : <votesmart_id>,
         ...
     }
-    
+
     """
+    source = StringField(required=True, help_text="Name of data source for this file, preferably standardized filename from datasource.py")
+    election_id = StringField(required=True, help_text="election id, e.g. md-2012-11-06-general")
+    contest = ReferenceField(Contest, reverse_delete_rule=CASCADE, required=True)
+    contest_slug = StringField(required=True, help_text="Denormalized contest slug for easier querying and obj repr")
+    # Keep state denormalized to ease querying?
+    state = StringField(max_length=2, required=True, help_text="Capitalized postal code")
+    #TODO: Add validation to require raw_full_name or raw_family_name
+    raw_full_name = StringField(max_length=300, required=True)
+    slug = StringField(max_length=300, required=True, help_text="Slugified name for easier querying and obj repr")
+    raw_given_name = StringField(max_length=200)
+    raw_family_name = StringField(max_length=200)
+    raw_suffix = StringField(max_length=200)
+    raw_parties = ListField(StringField(), default=list)
 
-class Office(Document):
-    state = StringField()
-    name = StringField()
-    district = StringField()
+    # FIELDS FOR TRANSFORMED/CLEANED DATA or LINKS TO OTHER DATA SETS
+    #name = StringField(max_length=300, required=True)
+    given_name = StringField(max_length=200)
+    family_name = StringField(max_length=200)
+    suffix = StringField(max_length=200)
+    additional_name = StringField(max_length=200)
+    other_names = ListField(StringField(), default=list)
+    parties = ListField(StringField(), default=list) # normalized? abbreviations?
+    identifiers = DictField()
 
-class Result(EmbeddedDocument):
+    def __unicode__(self):
+        name =  u'%s - %s' % (self.name, self.contest_slug)
+        parties = ""
+        if self.raw_parties:
+            parties = ", ".join([party for party in self.raw_parties])
+            if parties:
+                name += " (%s)" % paries
+        return name
+
+    @property
+    def name(self):
+        if self.raw_full_name:
+            name =  self.raw_full_name
+        else:
+            name = "%s, %s" % (self.raw_family_name, self.raw_given_name)
+            if self.raw_suffix:
+                name += " %s" % self.raw_suff
+        return name
+
+class Result(DynamicDocument):
 
     REPORTING_LEVEL_CHOICES = (
         'congressional_district',
@@ -57,32 +114,24 @@ class Result(EmbeddedDocument):
         'county',
         'state',
     )
-    jurisdiction = StringField()
-    ocd_id = StringField()
-    raw_office = StringField()
-    reporting_level = StringField(required=True)
-    candidate = EmbeddedDocumentField(Candidate)
+    source = StringField(required=True, help_text="Name of data source for this file, preferably standardized filename from datasource.py")
+    election_id = StringField(required=True, help_text="election id, e.g. md-2012-11-06-general")
+    contest = ReferenceField(Contest, reverse_delete_rule=CASCADE, required=True)
+    candidate = ReferenceField(Candidate, reverse_delete_rule=CASCADE, required=True)
+    contest_slug = StringField(required=True, help_text="Denormalized contest slug for easier querying and obj repr")
+    candidate_slug = StringField(required=True, help_text="Denormalized candidate slug for easier querying and obj repr")
+    #TODO: Do we need jurisdiction here if we have OCD id? And if so, should it be simply the raw name?
+    #jurisdiction = StringField(help_text="Political geography of this result, such as a County name, congressional district, state, etec.")
+    ocd_id = StringField(required=True)
+    reporting_level = StringField(required=True, choices=REPORTING_LEVEL_CHOICES)
+    raw_total_votes = IntField(required=True)
+    raw_winner = BooleanField()
+
+    # Should this write-in field be raw?
     write_in = BooleanField(default=False)
-    office = ReferenceField(Office)
-    party = StringField()
     total_votes = IntField(default=0)
-    vote_breakdowns = DictField() # if vote totals are included for election day, absentee, provisional, etc, put them here.
-    winner = BooleanField()
-    
-class Contest(Document):
-    """
-    Do we need other fields (state, year) on this for easy lookups?
-    
-    other_vote_counts would include provisional, absentee, same-day, overvotes, etc. all are optional.
-    """
-    state = StringField(required=True)
-    year = IntField(required=True)
-    election_id = StringField(required=True) # OpenElections generated slug
-    start_date = DateTimeField()
-    end_date = DateTimeField()
-    election_type = StringField()
-    result_type = StringField()
-    special = BooleanField(default=False)
-    results = ListField(EmbeddedDocumentField(Result))
-    created = DateTimeField()
-    updated = DateTimeField()
+    raw_vote_breakdowns = DictField(help_text="If provided, store vote totals for election day, absentee, provisional, etc.")
+    winner = BooleanField(help_text="Winner as determined by OpenElex, if not provided natively in data")
+
+    def __unicode__(self):
+        return u'%s-%s-%s' % (self.contest_slug, self.candidate_slug, self.result_slug)
