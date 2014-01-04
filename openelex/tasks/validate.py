8ad085bcc521a3d80eb07da579d783cbbfd65a33
Serdar Tumgoren <zstumgoren@gmail.com>
2014-01-04 12:14:36 -0800

    add validate.list task and tweak validate.run task

--- openelex/tasks/validate.py
+++ openelex/tasks/validate.py
@@ -5,6 +5,21 @@ from invoke import task
 
 from .utils import load_module
 
+
+@task(help={
+    'state':'Two-letter state-abbreviation, e.g. NY',
+})
+def list(state):
+    state_mod = load_module(state, ['validate'])
+    print "\nAvailable validators:\n"
+    for name in dir(state_mod.validate):
+        if name.startswith('validate_'):
+            func = getattr(state_mod.validate, name)
+            out = "\t%s" % name
+            if func.func_doc:
+                out += "\n\t\t %s" % func.func_doc
+            print out + "\n"
+
 @task(help={
     'state':'Two-letter state-abbreviation, e.g. NY',
 })
@@ -17,13 +32,14 @@ def run(state):
     state_mod = load_module(state, ['validate'])
     passed = []
     failed = []
+    print
     for name in dir(state_mod.validate):
         if name.startswith('validate_'):
             func = getattr(state_mod.validate, name)
             try:
                 func()
                 passed.append(name)
-            except AssertionError, e:
+            except Exception as e:
                 failed.append("Error: %s - %s - %s" % (state.upper(), name, e))
 
     print "\n\nVALIDATION RESULTS"
@@ -31,3 +47,5 @@ def run(state):
     print "Failed: %s" % len(failed)
     for fail in failed:
         print "\t%s" % fail
+    print
+
