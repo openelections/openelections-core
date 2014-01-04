86eea93328a0b66bc0b8013ec0439ad0a5a13b91
Serdar Tumgoren <zstumgoren@gmail.com>
2014-01-04 01:23:04 -0800

    update validate task

--- openelex/tasks/validate.py
+++ openelex/tasks/validate.py
@@ -15,10 +15,19 @@ def run(state):
     State is required.
     """
     state_mod = load_module(state, ['validate'])
+    passed = []
+    failed = []
     for name in dir(state_mod.validate):
         if name.startswith('validate_'):
             func = getattr(state_mod.validate, name)
             try:
                 func()
+                passed.append(name)
             except AssertionError, e:
-                sys.exit("Error: %s - %s - %s" % (state.upper(), name, e))
+                failed.append("Error: %s - %s - %s" % (state.upper(), name, e))
+
+    print "\n\nVALIDATION RESULTS"
+    print "Passed: %s" % len(passed)
+    print "Failed: %s" % len(failed)
+    for fail in failed:
+        print "\t%s" % fail
