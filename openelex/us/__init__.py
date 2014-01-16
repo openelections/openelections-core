d1b0acaaf73eef6b4bfa15b01e90462675789e2c
Serdar Tumgoren <zstumgoren@gmail.com>
2014-01-15 20:33:09 -0800

    fix import bug

--- openelex/us/__init__.py
+++ openelex/us/__init__.py
@@ -3,7 +3,10 @@ import sys
 # This is a pythonpath hack to avoid shadowing third-party 
 # module with our openelex.us directory
 from openelex import PROJECT_ROOT
-sys.path.remove(PROJECT_ROOT)
+try:
+    sys.path.remove(PROJECT_ROOT)
+except ValueError:
+    pass
 
 from us import STATES
 STATE_POSTALS = map(lambda state: state.abbr, STATES)
