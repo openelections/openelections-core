ef229d564038de179bb5c284e370e247456a1acb
Geoff Hing <geoffhing@gmail.com>
2014-02-13 12:28:21 -0600

    Refactor to allow passing filtering/limiting args to each method
    Given the long goal of filtering/limiting across states, remove
    state as a required argument to the constructor and accept
    filtering arguments to the different methods, imagining an
    eventual chainable interface that breaks out filtering, limiting
    and writing as separate calls.
    
    I don't know if this is the best way to do it, but it makes the
    most sense to me today.
    
    Addresses #39

--- openelex/base/bake.py
+++ openelex/base/bake.py
@@ -17,34 +17,13 @@ class Baker(object):
     Defaults to a version of ISO-8601 without '-' or ':' characters.
     """
 
-    def __init__(self, state, outputdir=None, **kwargs):
+    def __init__(self, **kwargs):
         """
         Constructor.
-
-        Arguments:
-
-        * state: Required. Postal code for a state.  For example, "md".
-        * outputdir: Directory where output files will be written. Defaults to 
-          "openelections/us/bakery"
-        * datefilter: Date specified in "YYYY" or "YYYY-MM-DD" used to filter
-          elections before they are baked.
-        * type: Election type. For example, general, primary, etc. 
-        * level: Reporting level of the election results.  For example, "state",
-          "county", "precinct", etc. Value must be one of the options specified
-          in openelex.models.Result.REPORTING_LEVEL_CHOICES.
-
         """
-        self.state = state.upper()
-        # kwargs will mostly be used to construct filters.  Just save them as
-        # an attribute so they're available for output filenames.
         self.kwargs = kwargs
-        self.outputdir = outputdir if outputdir else self.default_outputdir()
-
-        # TODO: Implement filtering by office, district and party after the 
-        # the data is standardized
-
-        # TODO: Sensible defaults for filtering.  By default, should filter to all
-        # state/contest-wide results for all races when no filters are specified.
+        # TODO: Decide if anything needs to happen here.
+        
 
     def default_outputdir(self):
         """
@@ -62,10 +41,17 @@ class Baker(object):
     # These hooks might be overkill but they help make the code more
     # ORM agnostic and easier to test.
 
-    def get_list(self):
-        filters = self.build_filters()
-        fields = self.build_fields()
-        return self.apply_filters(filters, fields)
+    def build_list(self, **kwargs):
+        limiting_kwargs = self.kwargs
+        limiting_kwargs.update(kwargs)
+        
+        filters = self.build_filters(**limiting_kwargs)
+        fields = self.build_fields(**limiting_kwargs)
+        self._objects = self.get_object_list()
+        self._objects = self.apply_filters(self._objects, filters)
+        self._objects = self.apply_field_limits(self._objects, fields)
+
+        return self 
 
     def build_date_filters(self, datestring):
         # QUESTION: Is it better to filter on one of the related fields
@@ -80,62 +66,93 @@ class Baker(object):
 
         return filters
  
-    def build_filters(self):
+    def build_filters(self, **kwargs):
         """
         Returns a dictionary of filter arguments that will be used to limit
         the queryset.
+
+        This allows for translating arguments from upstream code to the
+        filter format used by the underlying data store abstraction.
+
+        Arguments:
+
+        * state: Required. Postal code for a state.  For example, "md".
+        * outputdir: Directory where output files will be written. Defaults to 
+          "openelections/us/bakery"
+        * datefilter: Date specified in "YYYY" or "YYYY-MM-DD" used to filter
+          elections before they are baked.
+        * type: Election type. For example, general, primary, etc. 
+        * level: Reporting level of the election results.  For example, "state",
+          "county", "precinct", etc. Value must be one of the options specified
+          in openelex.models.Result.REPORTING_LEVEL_CHOICES.
+          
         """
-        # TODO: Finishing Implementing this
+        # TODO: Implement filtering by office, district and party after the 
+        # the data is standardized
+
+        # TODO: Sensible defaults for filtering.  By default, should filter to all
+        # state/contest-wide results for all races when no filters are specified.
         filters = {
-            'state': self.state,
+            'state': kwargs.get('state').upper(),
         }
 
         try:
-            filters.update(self.build_date_filters(self.kwargs.get('datefilter')))
+            filters.update(self.build_date_filters(kwargs.get('datefilter')))
         except AttributeError:
             pass
 
         return filters
 
-    def build_fields(self):
+    def build_fields(self, **kwargs):
         """
         Returns a list of fields that will be included in the result or an
-        empty list.
+        empty list to include all fields.
         """
         return [] 
 
-    def apply_filters(self, filters={}, fields=[]):
-        objects = self.get_object_list().filter(**filters).only(*fields)
-        return objects
+    def apply_filters(self, objects, filters={}):
+        return objects.filter(**filters)
+    
+    def apply_field_limits(self, objects, fields=[]):
+        return objects.only(*fields)
 
     def get_object_list(self):
         # QUESTION: Is this right, or do we need to merge across datasets?
+        # ANSWER: Flatten related models.
         return Result.objects
 
     def filename(self, **kwargs):
         timestamp = kwargs.get('timestamp', datetime.now())
         fmt = kwargs.get('fmt', 'csv')
-        return "%s_%s.%s" % (self.state,
+        state = self.kwargs.get('state')
+        return "%s_%s.%s" % (state,
             timestamp.strftime(self.timestamp_format), fmt) 
 
     def manifest_filename(self, **kwargs):
         timestamp = kwargs.get('timestamp', datetime.now())
-        return "%s_%s_manifest.txt" % (self.state,
+        state = self.kwargs.get('state')
+        return "%s_%s_manifest.txt" % (state,
             timestamp.strftime(self.timestamp_format)) 
            
-    def write(self, fmt='csv'):
+    def write(self, fmt='csv', outputdir=None):
         try:
             fmt_method = getattr(self, 'write_' + fmt) 
         except AttributeError:
             raise UnsupportedFormatError("Format %s is not supported" % (fmt))
+        
+        if outputdir is None:
+            outputdir = self.default_outputdir()
 
-        fmt_method()
+        fmt_method(outputdir)
 
-    def write_csv(self):
+    def write_csv(self, outputdir):
         raise NotImplementedError
 
-    def write_json(self):
+    def write_json(self, outputdir):
         raise NotImplementedError
 
-    def write_manifest(self):
+    def write_manifest(self, outputdir):
+        if outputdir is None:
+            outputdir = self.default_outputdir()
+
         raise NotImplementedError
--- openelex/tests/test_baker.py
+++ openelex/tests/test_baker.py
@@ -17,24 +17,24 @@ class TestBakeStateFileTask(TestCase):
 
 class TestBaker(TestCase):
     def test_filename(self):
-        baker = Baker('md')
+        baker = Baker(state='md')
         ts = datetime(2014, 2, 11, 10, 56, 15)
         fmt = 'json'
         filename = baker.filename(fmt=fmt, timestamp=ts)
         self.assertEqual(filename, 'md_20140211T105615.json')
 
     def test_manifest_filename(self):
-        baker = Baker('md')
+        baker = Baker(state='md')
         ts = datetime(2014, 2, 11, 10, 56, 15)
         filename = baker.manifest_filename(timestamp=ts)
         self.assertEqual(filename, 'md_20140211T105615_manifest.txt')
 
     def test_write_unsupported_format(self):
-        baker = Baker('md')
+        baker = Baker(state='md')
         self.assertRaises(UnsupportedFormatError, baker.write, 'xml')
 
     def test_default_outputdir(self):
-        baker = Baker('md')
+        baker = Baker(state='md')
         path = os.path.join(os.path.join('openelex', 'us', 'bakery'))
         outputdir = baker.default_outputdir()
         self.assertTrue(outputdir.endswith(path))
