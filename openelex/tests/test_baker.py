d0fc4cffad0297533f56b4b2e0eb5c047a1c0ef8
Geoff Hing <geoffhing@gmail.com>
2014-02-19 09:59:55 -0600

    More bakery tests
    This is a checkpoint commit for some bakery tests.
    
    Addresses #39

--- openelex/tests/test_baker.py
+++ openelex/tests/test_baker.py
@@ -3,33 +3,71 @@ from datetime import datetime
 from unittest import TestCase
 
 #from mongoengine.context_managers import query_counter
+from mongoengine import Q
 
 from openelex.exceptions import UnsupportedFormatError
 from openelex.tests.mongo_test_case import MongoTestCase
 from openelex.tests.factories import (ContestFactory, CandidateFactory,
     ResultFactory)
 
-from openelex.models import Contest, Candidate
+from openelex.models import Contest, Candidate, Result
 from openelex.base.bake import Roller, Baker
 
 
 class TestRoller(MongoTestCase):
+    """
+    Data-bound tests for the Roller class.
+    """
+
+    def _create_models(self, start_date, state="MD", election_type="general"):
+        """Create some election models for a given date"""
+        contest = ContestFactory(start_data=start_date,
+            election_type=election_type)
+        candidate = CandidateFactory(contest=contest)
+        result = ResultFactory(candidate=candidate, contest=contest)
+
     def setUp(self):
         # Call super to select the test database
         super(TestRoller, self).setUp()
 
         # Create some test models 
-        contest = ContestFactory()
-        candidate = CandidateFactory(contest=contest)
-        result = ResultFactory(candidate=candidate, contest=contest)
+        self._create_models(datetime(2012, 11, 6), election_type="general")
+        self._create_models(datetime(2012, 4, 3), election_type="primary")
+
+        self.roller = Roller()
+
+    def test_primary_collection_name(self):
+        self.assertEqual(self.roller.primary_collection_name, "result")
+
+    def _test_date_filter_matches(self, datestring, election_id="md-2012-11-06-general"):
+        """
+        Test that the query filter built for a given datestring matches an
+        election_id.
+        """
+        q = self.roller.build_date_filters(datestring)
+        self.assertTrue(isinstance(q, Q))
+        q_dict = q.to_query(Result)
+        self.assertIn('election_id', q_dict)
+        # The election_id value in the dict should be a regex that matches an
+        # election for that date 
+        m = q_dict['election_id'].search(election_id)
+        self.assertIsNotNone(m)
+
+    def test_build_date_filters(self):
+        # Test that different supported date formats build working filters
+        self._test_date_filter_matches("2012")
+        self._test_date_filter_matches("201211")
+        self._test_date_filter_matches("20121106")
+
+        # Test that a ValueError is raised for an unsupported date format
+        self.assertRaises(ValueError, self.roller.build_date_filters, "201200")
 
     def test_get_list(self):
-        roller = Roller()
-        data = roller.get_list(state='md', datefilter='20120403',
-                type='general', level='state_legislative')
-        from pprint import pprint
-        pprint(data[0])
-        self.fail()
+        data = self.roller.get_list(state='md', datefilter='20121106',
+            type='general')
+        self.assertEqual(len(data),
+            Result.objects(election_id__contains='md-2012-11-06-general').count())
+        # TODO: Test this further
 
 class TestBaker(TestCase):
     def test_filename(self):
@@ -54,11 +92,3 @@ class TestBaker(TestCase):
         path = os.path.join(os.path.join('openelex', 'us', 'bakery'))
         outputdir = baker.default_outputdir()
         self.assertTrue(outputdir.endswith(path))
-
-    def test_collect_items(self):
-        baker = Baker(state='md', datefilter='20120403',
-                type='general', level='state_legislative')
-        data = baker.collect_items().get_items()
-        print data[0]
-            
-        self.fail()
