2e45a52b5ea8a8de10a7091635c3dbc3563d9ee5
Geoff Hing <geoffhing@gmail.com>
2014-03-02 10:02:44 -0500

    Checkpoint commit for transforming MD RawResults
    Addresses #49

--- openelex/us/md/transform/__init__.py
+++ openelex/us/md/transform/__init__.py
@@ -15,8 +15,30 @@ Map of party abbreviations as they appear in MD raw results to canonical
 abbreviations.
 """
 
+# Lists of fields on RawResult that are contributed to the canonical
+# models.  Maybe this makes more sense to be in the model.
+
+# These get copied onto all the related models
+meta_fields = ['source', 'election_id', 'state',]
+contest_fields = meta_fields + ['start_date', 'end_date',
+    'election_type', 'primary_type', 'result_type', 'special',]
+candidate_fields = meta_fields + ['full_name',]
+result_fields = meta_fields + ['reporting_level',]
+
+# Caches to avoid hitting the database
 office_cache = {}
 party_cache = {}
+
+def _get_fields(raw_result, field_names):
+    """
+    Extract the fields from a RawResult that will be used to
+    construct a related model.
+
+    Returns a dict of fields and values that can be passed to the
+    model constructor.
+    """
+    return { k:getattr(raw_result, k) for k in field_names } 
+
 
 def _clean_office(office):
     if "president" in office.lower():
@@ -65,8 +87,12 @@ def _get_office(raw_result):
         office_cache[key] = office
         return office
 
-def _get_party(raw_result):
-    clean_abbrev = _clean_party(raw_result.party)
+def _get_party(raw_result, attr='party'):
+    party = getattr(raw_result, attr)
+    if not party:
+        return None
+
+    clean_abbrev = _clean_party(party)
     try:
         return party_cache[clean_abbrev]
     except KeyError:
@@ -74,46 +100,67 @@ def _get_party(raw_result):
         party_cache[clean_abbrev] = party
         return party
 
-def create_unique_contests_after_2002():
-    timestamp = datetime.now()
+# TODO: What should we do with existing records?
+
+def get_raw_results_after_2002():
     # Filter raw results for everything newer than 2002
-    raw_results = RawResult.objects.filter(state='MD', end_date__gte=datetime(2003, 1, 1))
-    contests = []
+    return RawResult.objects.filter(state='MD', end_date__gte=datetime(2003, 1, 1))
+
+def get_or_create_contest(raw_result):
+    # Resolve Office and Party related objects
+    fields = _get_fields(raw_result, contest_fields)
+    if not fields['primary_type']:
+        del fields['primary_type']
+    fields['office'] = _get_office(raw_result)
+    fields['primary_party'] = _get_party(raw_result, 'primary_party')
+
+    # Create Contest
+    return Contest.objects.get_or_create(**fields)
+
+def create_unique_contests_after_2002():
+    num_created = 0
+
     #import ipdb;ipdb.set_trace()
-    for rr in raw_results:
-        # Resolve Office and Party related objects
-        office = _get_office(rr)
-        party = _get_party(rr)
+    for rr in get_raw_results_after_2002():
+        (contest, created) = get_or_create_contest(rr)
+        if created:
+            num_created += 1
 
-        kwargs = rr._data.copy()
-        kwargs.pop('id')
-        kwargs.pop('created')
-        kwargs.pop('updated')
-        # Create Contest
-        #contest = Contest(**kwargs)
-        #contests.append(contest)
+    print "Created %d contests." % num_created
 
-    #TODO: save Contest instances
+def get_or_create_candidate(raw_result):
+    (contest, created) = get_or_create_contest(raw_result)
+    assert not created
+    fields = _get_fields(raw_result, candidate_fields)
+    fields['contest'] = contest
+    name = HumanName(raw_result.full_name)
+    fields['given_name'] = name.first
+    fields['family_name'] = name.last
+    fields['additional_name'] = name.middle
+    fields['suffix'] = name.suffix
+    return Candidate.objects.get_or_create(**fields)
 
-#TODO: create_unique_candidates_after_2002
-#TODO: create_unique_results_after_2002
-
-def parse_names_after_2002():
-    cands = Candidate.objects.filter(
-        (Q(election_id__not__contains="2000") | Q(election_id__not__contains="2002")),
-        state='MD'
-    )
-    #Loop through candidates, perform name parse and update
-    for cand in cands:
-        # Skip Other write-ins
-        if 'other' in cand.slug:
+def create_unique_candidates_after_2002():
+    for rr in get_raw_results_after_2002():
+        # TODO: Is this the right way to handle "Other Write-Ins"
+        if "other" in rr.full_name.lower():
             continue
-        name = HumanName(cand.raw_full_name)
-        cand.given_name = name.first
-        cand.family_name = name.last
-        cand.additional_name = name.middle
-        cand.suffix = name.suffix
-        cand.save()
+
+        (candidate, created) = get_or_create_candidate(rr)
+   
+def create_unique_results_after_2002(self):
+    for rr in get_raw_results_after_2002():
+        fields = _get_fields(rr, result_fields)
+        fields['candidate'] = get_or_create_candidate(rr)
+        fields['contest'] = fields['candidate'].contest 
+        fields['raw_result'] = rr
+        fields['party'] = _get_party(rr)
+        # TODO: Make sure other fields end up in result. Will
+        # probably also have to add them to rawresult
+        # BOOKMARK
+        Result.objects.get_or_create(**fields)
+
+# TODO: When should we create a Person
 
 #def standardize_office_and_district():
 #    pass
@@ -122,6 +169,7 @@ def parse_names_after_2002():
     #pass
 
 registry.register('md', create_unique_contests_after_2002)
-registry.register('md', parse_names_after_2002)
+registry.register('md', create_unique_candidates_after_2002)
+registry.register('md', create_unique_results_after_2002)
 #registry.register('md', standardize_office_and_district)
 #registry.register('md', clean_vote_counts)
