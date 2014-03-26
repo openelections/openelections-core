2aedbc215b5385f42a687b14448c43227bc47d76
Geoff Hing <geoffhing@gmail.com>
2014-03-26 17:38:13 -0500

    Add validator for aggregating districty by county results
    Add a validator for the aggregation done by Maryland's
    ``create_district_results_from_county_splits`` transform.
    
    Addresses https://github.com/openelections/core/issues/36

--- openelex/us/md/validate.py
+++ openelex/us/md/validate.py
@@ -1,4 +1,4 @@
-from openelex.models import Contest, Candidate, Result
+from openelex.models import Office, Contest, Candidate, Result
 
 #TODO: Add generic test for unique candidacies per contest
 #TODO: Add Result validations
@@ -57,6 +57,73 @@ def validate_obama_primary_candidacy_2012():
         raise Candidate.DoesNotExist("zero obama primary candidacies found for 2012")
     except Candidate.MultipleObjectsReturned as e:
         raise Candidate.MultipleObjectsReturned("multiple obama primary candidacies found for 2012: %s" %  e)
+
+def validate_result_count_2012_general_state_legislative():
+    """Should be 5504 results for the 2012 general election at the state legislative district level""" 
+    filter_kwargs = {
+        'state': 'MD',
+        'election_id': 'md-2012-11-06-general',
+        'reporting_level': 'state_legislative',
+    }
+    assert Result.objects.filter(**filter_kwargs).count() == 5504
+
+def validate_aggregate_congressional_district_results():
+    """Validate that results have been correctly aggregated from congressional districts split by county"""
+
+    election_id = 'md-2000-03-07-primary'
+
+    # President
+    contest = Contest.objects.get(election_id=election_id, slug='president-d')
+    results = Result.objects.filter(election_id=election_id,
+        contest=contest, reporting_level='congressional_district')
+    # Maryland has 8 congressional districts
+    assert len(results.distinct('jurisdiction')) == 8
+    # 4 candidates * 8 districts = 32 results
+    assert results.count() == 32
+    # Al Gore got 32426 votes in district 1
+    candidate = Candidate.objects.get(slug='al-gore', election_id=election_id)
+    assert results.get(candidate=candidate, jurisdiction='1').votes == 32426
+
+    # U.S. House 
+    contest = Contest.objects.get(election_id=election_id,
+        slug='us-house-of-representatives-1-r')
+    results = Result.objects.filter(election_id=election_id, contest=contest,
+        reporting_level='congressional_district')
+    # Only 1 candidate in 1 district
+    assert results.count() == 1
+    # Wayne T. Gilchrest got 49232 votes
+    candidate = Candidate.objects.get(slug='wayne-t-gilchrest',
+        election_id=election_id)
+    assert results.get(candidate=candidate, jurisdiction='1').votes == 49232 
+
+    election_id = 'md-2008-02-12-primary'
+    
+    # President
+    contest = Contest.objects.get(election_id=election_id, slug='president-d')
+    results = Result.objects.filter(election_id=election_id,
+        contest=contest, reporting_level='congressional_district')
+    # Maryland has 8 congressional districts
+    assert len(results.distinct('jurisdiction')) == 8
+    # 9 candidates * 8 districts = 72 results
+    assert results.count() == 72
+    candidate = Candidate.objects.get(slug='hillary-clinton',
+        election_id=election_id)
+    assert results.get(candidate=candidate, jurisdiction='6').votes == 34322 
+
+    election_id = 'md-2012-04-03-primary'
+
+    # President
+    contest = Contest.objects.get(election_id=election_id, slug='president-d')
+    results = Result.objects.filter(election_id=election_id,
+        contest=contest, reporting_level='congressional_district')
+    # Maryland has 8 congressional districts
+    assert len(results.distinct('jurisdiction')) == 8
+    # 2 candidates * 8 disctricts = 16 results
+    assert results.count() == 16
+    candidate = Candidate.objects.get(slug='barack-obama',
+        election_id=election_id)
+    assert results.get(candidate=candidate, jurisdiction='1').votes == 20343
+
 
 #def validate_name_parsing():
     #Check assortment of names
