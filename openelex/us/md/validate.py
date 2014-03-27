ae18b47f3b10f937291a55ee02fb253c400eded6
Geoff Hing <geoffhing@gmail.com>
2014-03-27 10:49:58 -0500

    Add validator for county congressional results
    Add a validator, ``validate_2000_primary_congress_county_results``,
    that checks that county-level results for congressional offices in the
    2000-03-07 Maryland primary have been created.
    
    This validates the ``create_2000_primary_congress_county_results``
    transform.
    
    Addresses https://github.com/openelections/core/issues/36

--- openelex/us/md/validate.py
+++ openelex/us/md/validate.py
@@ -124,6 +124,36 @@ def validate_aggregate_congressional_district_results():
         election_id=election_id)
     assert results.get(candidate=candidate, jurisdiction='1').votes == 20343
 
+def validate_2000_primary_congress_county_results():
+    """Confirm that county level results are created for congressional races in the 2000 primary"""
+    results = Result.objects.filter(state='MD',
+        reporting_level='county',
+        election_id='md-2000-03-07-primary',
+        contest_slug__startswith='us-house-of-representatives')
+
+    district_1_results_d = results.filter(contest_slug='us-house-of-representatives-1-d')
+    # 11 counties intersect with district 1 * 4 Democratic candidates = 44
+    # results
+    count = district_1_results_d.count()
+    assert count  == 44, ("There should be 44 results for District 1, instead"
+        "there are %d" % count) 
+    # Bennett Bozman got 3429 votes in Worcester county
+    result = results.get(candidate_slug='bennett-bozman',
+        jurisdiction='Worcester')
+    assert result.votes == 3429, ("Bennett Bozman should have 3429 votes in "
+        "Worcester County, instead has %d" % result.votes) 
+
+    district_8_results_r = results.filter(contest_slug='us-house-of-representatives-8-r')
+    # 1 county intersects with district 8 * 1 Republican candidate = 1
+    count = district_8_results_r.count()
+    assert count == 1, ("There should be 1 result for District 8, instead "
+        "there are %d" % count)
+    # Constance A. Morella got 35472 votes in Montgomery county 
+    result = results.get(candidate_slug='constance-a-morella',
+        jurisdiction='Montgomery')
+    assert result.votes == 35472, ("Constance A. Morella should have 35472 "
+        "votes in Montgomery county.  Instead has %d" % result.votes)
+
 
 #def validate_name_parsing():
     #Check assortment of names
