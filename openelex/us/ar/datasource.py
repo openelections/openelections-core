4e22018261f6d76e97a6a11f4e1b534a47c1e6b9
Geoff Hing <geoffhing@gmail.com>
2014-05-01 23:18:48 -0500

    Mappings for Clarity results
    Produce mappings for results (2012+) that are produced by the
    Clarity system.
    
    Addresses https://github.com/openelections/core/issues/124

--- openelex/us/ar/datasource.py
+++ openelex/us/ar/datasource.py
@@ -2,6 +2,8 @@ import os.path
 import re
 import urlparse
 
+from bs4 import BeautifulSoup
+import requests
 import unicodecsv
 
 from openelex import PROJECT_ROOT
@@ -11,7 +13,12 @@ from openelex.lib.text import slugify, election_slug, ocd_type_id
 
 class Datasource(BaseDatasource):
     RESULTS_PORTAL_URL = "http://www.sos.arkansas.gov/electionresults/index.php"
-    
+    CLARITY_PORTAL_URL = "http://results.enr.clarityelections.com/AR/"
+
+    def __init__(self, state=''):
+        super(Datasource, self).__init__(state)
+        self._cached_url_paths = {}
+
     def mappings(self, year=None):
         mappings = []
         for yr, elecs in self.elections(year).items():
@@ -39,6 +46,7 @@ class Datasource(BaseDatasource):
         Return a list of metadata entries for a single election.
         """
         slug = election['slug']
+        link = election['direct_links'][0]
 
         if slug == 'ar-2000-11-07-general':
             return self._build_election_metadata_2000_general(election)
@@ -47,6 +55,8 @@ class Datasource(BaseDatasource):
                 'ar-2001-10-16-special-primary-runoff',
                 'ar-2001-11-20-special-general'):
             return self._build_election_metadata_zipped_special(election)
+        elif link.startswith(self.CLARITY_PORTAL_URL):
+            return self._build_election_metadata_clarity(election)
         else:
             return self._build_election_metadata_default(election)
 
@@ -114,7 +124,7 @@ class Datasource(BaseDatasource):
             })
         return meta_entries
 
-    def _standardized_filename(self, election, **kwargs):
+    def _standardized_filename(self, election, bits=[], **kwargs):
         reporting_level = kwargs.get('reporting_level', None)
         jurisdiction = kwargs.get('jurisdiction', None)
         office = kwargs.get('office', None)
@@ -122,10 +132,10 @@ class Datasource(BaseDatasource):
         extension = kwargs.get('extension',
             self._filename_extension(election))
 
-        bits = [
+        bits.extend([
             election['start_date'].replace('-', ''),
             self.state,
-        ]
+        ])
 
         if election['special']:
             bits.append('special')
@@ -156,24 +166,126 @@ class Datasource(BaseDatasource):
         root, ext = os.path.splitext(parts.path)
         return ext
 
+    def _build_election_metadata_clarity(self, election, fmt="xls"):
+        """
+        Return metadata entries for election results provided by the Clarity
+        system.
+
+        These results seem to be for elections starting in 2012.
+        """
+        return self._build_election_metadata_clarity_county(election, fmt) +\
+            self._build_election_metadata_clarity_precinct(election, fmt)
+        
+    def _build_election_metadata_clarity_county(self, election, fmt):
+        base_url = self._clarity_election_base_url(election['direct_links'][0])
+
+        return [{
+            "generated_filename": self._standardized_filename(election,
+                reporting_level='county', extension='.'+fmt),
+            "raw_url": self._clarity_results_url(base_url, fmt), 
+            "ocd_id": 'ocd-division/country:us/state:ar',
+            "name": 'Arkansas',
+            "election": election['slug']
+        }]
+
+    def _build_election_metadata_clarity_precinct(self, election, fmt):
+        meta_entries = []
+        for path in self._clarity_precinct_url_paths(election, fmt):
+            jurisdiction = path['jurisdiction']
+            ocd_id = 'ocd-division/country:us/state:ar/county:{}'.format(ocd_type_id(jurisdiction))
+            filename = self._standardized_filename(election,
+                jurisdiction=jurisdiction, reporting_level='precinct', 
+                extension='.'+fmt)
+            meta_entries.append({
+                "generated_filename": filename,
+                "raw_url": path['url'],
+                "ocd_id": ocd_id, 
+                "name": jurisdiction, 
+                "election": election['slug'],
+            })
+        return meta_entries
+            
+    def _clarity_election_base_url(self, url):
+        return url.split('/en/')[0] + '/'
+
+    def _clarity_results_url(self, base_url, fmt):
+        return "{}/reports/detail{}.zip".format(base_url, fmt)
+
+    def _clarity_precinct_url_paths_filename(self, election):
+        filename = self._standardized_filename(election, ['url_paths'],
+            reporting_level='precinct', extension='.csv')
+        return os.path.join(PROJECT_ROOT, self.mappings_dir, filename)
+
+    def _clarity_precinct_url_paths(self, election, fmt):
+        url_paths_filename = self._clarity_precinct_url_paths_filename(election)
+        if os.path.exists(url_paths_filename):
+            return self._url_paths(url_paths_filename)
+
+        url_paths = []
+        for url, county in self._clarity_county_urls(election):
+            url_paths.append({
+                'date': election['start_date'],
+                'office': '',
+                'race_type': election['race_type'],
+                'party': '',
+                'special': election['special'],
+                'url': self._clarity_results_url(url, fmt),
+                'reporting_level': 'precinct',
+                'jurisdiction': county,
+            })
+
+        with open(url_paths_filename, 'wb') as f:
+            fieldnames = ['date', 'office', 'race_type', 'party',
+                'special', 'url', 'reporting_level', 'jurisdiction']
+            writer = unicodecsv.DictWriter(f, fieldnames)
+            writer.writerows(url_paths)
+
+        return url_paths
+
+    def _clarity_county_urls(self, election):
+        base_url = self._clarity_election_base_url(election['direct_links'][0])
+        county_url = base_url + 'en/select-county.html' 
+        r = requests.get(county_url)
+        r.raise_for_status()
+        return [(self._clarity_county_url(path), county) for path, county 
+                in self._scrape_county_paths(r.text)]
+
+    def _scrape_county_paths(self, html):
+        soup = BeautifulSoup(html)
+        return [(o['value'], o.get_text()) for o in soup.select("table li a")]
+
+    def _clarity_county_url(self, path):
+        url = self.CLARITY_PORTAL_URL + path.lstrip('/')
+        r = requests.get(url)
+        r.raise_for_status()
+        redirect_path = self._scrape_county_redirect_path(r.text)
+        return url + redirect_path
+
+    def _scrape_county_redirect_path(self, html):
+        soup = BeautifulSoup(html)
+        script_src = soup.find('script')['src']
+        match = re.search(r'\./(\d+)/.*', script_src)
+        return match.group(1) + '/summary.html'
+
     def _counties(self):
         county_ocd_re = re.compile(r'ocd-division/country:us/state:ar/county:[^/]+$')
         return [m for m in self.jurisdiction_mappings()
                 if county_ocd_re.match(m['ocd_id'])]
 
-    def _url_paths(self):
+    def _url_paths(self, filename=None):
         # TODO: Make this align with other state modules, perhaps move to
         # BaseDatasource
         try:
-            return self._cached_url_paths
-        except AttributeError:
-            filename = os.path.join(PROJECT_ROOT, self.mappings_dir, 'url_paths.csv')
-            self._cached_url_paths = []
+            return self._cached_url_paths[filename]
+        except KeyError:
+            if filename is None:
+                filename = os.path.join(PROJECT_ROOT, self.mappings_dir, 'url_paths.csv')
+            cached = self._cached_url_paths[filename] = []
             with open(filename, 'rU') as csvfile:
                 reader = unicodecsv.DictReader(csvfile)
                 for row in reader:
-                    self._cached_url_paths.append(self._parse_url_path(row))
-            return self._cached_url_paths
+                    cached.append(self._parse_url_path(row))
+            return cached 
 
     def _parse_url_path(self, row):
         clean_row = row.copy()
