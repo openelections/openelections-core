a5c145b8e9fb2c68843f5e655d221018d0999fef
Geoff Hing <geoffhing@gmail.com>
2014-06-02 10:25:39 -0500

    Clean up docstrings for openelex.lib

--- openelex/lib/__init__.py
+++ openelex/lib/__init__.py
@@ -1,5 +1,14 @@
 def build_github_url(state, generated_filename):
-    """Return a URL to the preprocessed files hosted on GitHub"""
+    """
+    Generate a URL to a preprocessed result file hosted on GitHub
+
+    Args:
+        generated_filename: Standardized filename of an election result file.
+
+    Returns:
+        String containing a URL to the preprocessed result file on GitHub.
+    
+    """
     tpl = "https://raw.githubusercontent.com/openelections/openelections-data-{}/master/{}"
     return tpl.format(state.lower(), generated_filename)
 
