685b229fd4467b7f1145c2f7a7609f860c91026d
Derek Willis <dwillis@gmail.com>
2014-06-14 22:31:54 -0500

    added raw_github_url method

--- openelex/lib/__init__.py
+++ openelex/lib/__init__.py
@@ -12,4 +12,16 @@ def build_github_url(state, generated_filename):
     tpl = "https://raw.githubusercontent.com/openelections/openelections-data-{}/master/{}"
     return tpl.format(state.lower(), generated_filename)
 
+def build_raw_github_url(state, raw_filename):
+    """
+    Generate a URL to a raw result file hosted on GitHub
 
+    Args:
+        raw_filename: Raw filename of an election result file.
+
+    Returns:
+        String containing a URL to the raw result file on GitHub.
+    
+    """
+    tpl = "https://raw.githubusercontent.com/openelections/openelections-data-{}/master/raw/{}"
+    return tpl.format(state.lower(), raw_filename)	
