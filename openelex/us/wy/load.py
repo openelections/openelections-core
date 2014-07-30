87c2209e4f2e9e1ac05d2803061a3d24df9cf99f
Derek Willis <dwillis@gmail.com>
2014-07-30 07:31:15 -0400

    primary file loader for 2010

--- openelex/us/wy/load.py
+++ openelex/us/wy/load.py
@@ -96,9 +96,10 @@ class WYLoader(WYBaseLoader):
             if self._skip_row(row): 
                 continue
             else:
-                grouped_results = zip(candidates, row[1:len(candidates)+1])
-                for (candidate, office), votes in grouped_results:
-                    results.append(self._prep_precinct_result(precinct, candidate, office, votes))
+                votes = [v for v in row[1:len(candidates)] if not v == '']
+                grouped_results = zip(candidates, votes)
+                for (candidate, office, party), votes in grouped_results:
+                    results.append(self._prep_precinct_result(precinct, candidate, office, party, votes))
         RawResult.objects.insert(results)
 
     def _skip_row(self, row):
@@ -117,7 +118,6 @@ class WYLoader(WYBaseLoader):
         else:
             offices = sheet.row_values(1)
         office_indexes = [offices.index(x) for x in offices if x != '' and " ".join(x.split()[0:2]).strip() in self.office_segments]
-        office_max_indexes = [o-1 for o in office_indexes[1:]]
         office_labels = [x for x in offices if " ".join(x.split()[0:2]).strip() in self.office_segments]
         new_offices = []
         for o in offices:
@@ -133,13 +133,35 @@ class WYLoader(WYBaseLoader):
     def _build_candidates(self, sheet):
         # map candidates to offices so we can lookup up one and get the other
         # for our purposes, candidates include totals for write-ins, over and under votes
+        # TODO: filter out write-ins, over and under votes
         offices = self._build_offices(sheet)
-        if sheet.row_values(0)[1] != '':
-            cands = sheet.row_values(1)
-        else:
+        if 'Republican' in sheet.row_values(1) or 'Democratic' in sheet.row_values(1):
             cands = sheet.row_values(2)
-        candidates = [c.replace('\n', ' ') for c in cands[1:-1]][:len(offices)]
-        return zip(candidates, offices)
+            candidates = [c for c in cands[1:-1] if not c == ''][:len(offices)]
+            raw_parties = sheet.row_values(1)[1:-1][:len(offices)]
+            parties = []
+            for p in raw_parties:
+                if p != '':
+                    previous = p
+                    parties.append(p)
+                elif p == '':
+                    parties.append(previous)
+                else:
+                    break
+        elif sheet.row_values(0)[1] != '':
+            cands = []
+            parties = []
+            raw_cands = sheet.row_values(1)
+            for cand in raw_cands:
+                if "(" in cand:
+                    parties.append(cand.split('(')[1].replace(')', '').strip())
+                    cands.append(cand.split('(')[0].replace('  ',' ').strip())
+                else:
+                    parties.append(None)
+                    cands.append(cand)
+            candidates = [c.replace('\n', ' ') for c in cands[1:-1]][:len(offices)]
+            parties = parties[1:-1][:len(offices)]
+        return zip(candidates, offices, parties)
 
     def _build_contest_kwargs(self, office):
         # find a district number, if one exists (state house & senate only)
@@ -151,42 +173,35 @@ class WYLoader(WYBaseLoader):
         kwargs = {
             'office': office.strip(),
             'district': district,
-            'primary_party': None
         }
         return kwargs
 
-    def _build_candidate_kwargs(self, candidate):
+    def _build_candidate_kwargs(self, candidate, party):
         # check if party is in name, extract if so
-        if "(" in candidate:
-            party = candidate.split('(')[1].replace(')', '').strip()
-            name = candidate.split('(')[0].replace('  ',' ').strip()
-        else:
-            party = None
-            name = candidate
-        full_name = name
+        full_name = candidate
         slug = slugify(full_name, substitute='-')
         kwargs = {
             'full_name': full_name,
-            'party': party,
             'name_slug': slug,
+            'party': party,
         }
         return kwargs
 
-    def _base_kwargs(self, candidate, office):
+    def _base_kwargs(self, candidate, office, party):
         "Build base set of kwargs for RawResult"
         kwargs = self._build_common_election_kwargs()
         contest_kwargs = self._build_contest_kwargs(office)
-        candidate_kwargs = self._build_candidate_kwargs(candidate)
+        candidate_kwargs = self._build_candidate_kwargs(candidate, party)
         kwargs.update(contest_kwargs)
         kwargs.update(candidate_kwargs)
         return kwargs
 
-    def _prep_precinct_result(self, precinct, candidate, office, votes):
+    def _prep_precinct_result(self, precinct, candidate, office, party, votes):
         # each precinct has multiple candidate totals, plus write-ins, over and under votes
         print candidate
-        if self._votes(votes) == None:
-            pass
-        kwargs = self._base_kwargs(candidate, office)
+        kwargs = self._base_kwargs(candidate, office, party)
+        if party:
+            kwargs.update({'primary_party': party})
         kwargs.update({
             'reporting_level': 'precinct',
             'jurisdiction': precinct,
