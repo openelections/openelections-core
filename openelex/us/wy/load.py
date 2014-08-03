da04a4f2ab3dda75a8ffe709e9590afeb4c1879c
Derek Willis <dwillis@gmail.com>
2014-08-02 22:34:28 -0400

    working loader for 2004 general, 2008 all

--- openelex/us/wy/load.py
+++ openelex/us/wy/load.py
@@ -23,7 +23,9 @@ class LoadResults(object):
 
     def run(self, mapping):
         election_id = mapping['election']
-        if any(s in election_id for s in ['2006', 'special']):
+        if election_id == 'wy-2008-11-25-special-general':
+            loader = WYSpecialLoader2008()
+        elif any(s in election_id for s in ['2006', 'special']):
             loader = WYLoaderCSV()
         else:
             loader = WYLoader()
@@ -81,30 +83,86 @@ class WYBaseLoader(BaseLoader):
 class WYLoader(WYBaseLoader):
     """
     Parse Wyoming election results for all elections except those in 2006 or special elections.
+    Works for:
+    2012 general & primary
+    2010 general & primary
+    2008 general & primary
+    2004 general
 
+    Doesn't work for:
+    2004 primary
+    2002
+    2000
     """
     def load(self):
+        year = int(re.search(r'\d{4}', self.election_id).group())
         xlsfile = xlrd.open_workbook(self._xls_file_handle())
+        if 'primary' in self._xls_file_handle():
+            primary = True
+            party = self._party_from_filehandle(self._xls_file_handle())
+        else:
+            primary = False
+            party = None
         results = []
-        sheet = xlsfile.sheet_by_name('Sheet1')
-        candidates = self._build_candidates(sheet)
+
+        sheet = self._get_sheet(xlsfile)
+        if year == 2004:
+            candidates = self._build_candidates_2004(sheet, party)
+        else:
+            candidates = self._build_candidates(sheet, party)
 
         for i in xrange(sheet.nrows):
-            row = sheet.row_values(i)
-            precinct = str(row[0])
+            row = [r for r in sheet.row_values(i) if not r == '']
+            # remove empty cells
             # Skip non-target offices
             if self._skip_row(row): 
                 continue
             else:
-                votes = [v for v in row[1:len(candidates)] if not v == '']
+                precinct = str(row[0])
+                if year == 2004:
+                    votes = [v for v in row[2:len(candidates)] if not v == '']
+                elif len(candidates) == 1:
+                    votes = [v for v in row[1:] if not v == '']
+                else:
+                    votes = [v for v in row[1:len(candidates)] if not v == '']
                 grouped_results = zip(candidates, votes)
-                for (candidate, office, party), votes in grouped_results:
+                for (candidate, office, candidate_party), votes in grouped_results:
                     if not votes == '-':
-                        results.append(self._prep_precinct_result(precinct, candidate, office, party, votes))
-        RawResult.objects.insert(results)
+                        results.append(self._prep_precinct_result(precinct, candidate, office, candidate_party, votes))
+        try:
+            RawResult.objects.insert(results)
+        except:
+            print grouped_results
+            raise
+
+    def _get_sheet(self, xlsfile):
+        if '2004' in self.source:
+            county = self.mapping['name']
+            if 'general' in self.source:
+                sheet_name = county + ' General'
+            sheet = xlsfile.sheet_by_name(sheet_name)
+        else:
+            try:
+                sheet = xlsfile.sheet_by_name('Sheet1')
+            except:
+                sheet = xlsfile.sheet_by_name('Party_PbP_Candidates_Summary')
+        return sheet
+
+    def _party_from_filehandle(self, file):
+        return file.split("__")[2].title()
 
     def _skip_row(self, row):
+        if row == []:
+            return True
         if row[0] == 'Total':
+            return True
+        if row[0] == 'Official Totals':
+            return True
+        if row[0].strip() == '':
+            return True
+        if row[0].startswith("* The State Canvassing Board"):
+            return True
+        if row[0].replace('\n',' ').strip() in self.target_offices:
             return True
         # if the contents of the second cell is not a float, skip that row
         try:
@@ -112,33 +170,74 @@ class WYLoader(WYBaseLoader):
             return False
         except ValueError:
             return True
+        except IndexError:
+            return True
+
+    def _build_offices_2004(self, sheet):
+        a = sheet.row_values(0)[2:]
+        b = sheet.row_values(1)[2:]
+        raw_offices = [" ".join(x) for x in zip(a,b)]
+        office_labels = [x for x in raw_offices if " ".join(x.split()[0:2]).strip() in self.office_segments]
+        office_labels = list(set(office_labels))
+        offices = []
+        for o in raw_offices:
+            if o in office_labels:
+                office = o.replace('\n', ' ').replace('  ',' ').strip()
+                previous = office
+                offices.append(office)
+            elif o.strip() == '':
+                offices.append(previous)
+            else:
+                continue
+        return offices
 
     def _build_offices(self, sheet):
         if sheet.row_values(0)[1] != '':
-            offices = sheet.row_values(0)[1:]
+            raw_offices = sheet.row_values(0)[1:]
         else:
-            offices = sheet.row_values(1)
-        office_indexes = [offices.index(x) for x in offices if x != '' and " ".join(x.split()[0:2]).strip() in self.office_segments]
-        office_labels = [x for x in offices if " ".join(x.split()[0:2]).strip() in self.office_segments]
-        new_offices = []
-        for o in offices:
+            raw_offices = sheet.row_values(1)[1:]
+        if raw_offices[0] == '' or raw_offices[0] == 'Total':
+            del raw_offices[0]
+        office_labels = [x for x in raw_offices if " ".join(x.split()[0:2]).strip() in self.office_segments]
+        office_labels = list(set(office_labels)) # unique it
+        offices = []
+        for o in raw_offices:
             if o in office_labels:
-                previous = o
-                new_offices.append(o)
+                previous = o.replace('\n', ' ')
+                offices.append(o.replace('\n', ' '))
             elif o == '':
-                new_offices.append(previous)
+                offices.append(previous)
             else:
                 break
-        return new_offices
+        return offices
 
-    def _build_candidates(self, sheet):
+    def _build_candidates_2004(self, sheet, party):
+        offices = self._build_offices_2004(sheet)
+        a = sheet.row_values(3)[2:]
+        b = sheet.row_values(4)[2:]
+        raw_cands = [" ".join(x) for x in zip(a,b)][:len(offices)]
+        candidates = []
+        parties = []
+        for cand in raw_cands:
+            if "(" in cand:
+                parties.append(cand.split('(')[1].replace(')', '').strip())
+                candidates.append(cand.split('(')[0].replace('  ',' ').strip())
+            else:
+                parties.append(party)
+                candidates.append(cand)
+        return zip(candidates, offices, parties)
+
+    def _build_candidates(self, sheet, party):
         # map candidates to offices so we can lookup up one and get the other
         # for our purposes, candidates include totals for write-ins, over and under votes
         # TODO: filter out write-ins, over and under votes
         offices = self._build_offices(sheet)
         if 'Republican' in sheet.row_values(1) or 'Democratic' in sheet.row_values(1):
-            cands = sheet.row_values(2)
-            candidates = [c for c in cands[1:-1] if not c == ''][:len(offices)]
+            cands = sheet.row_values(2)[1:-1]
+            # find indexes of empty cand cells and remove them from offices
+            empty_cells = [i for i , c in enumerate(cands) if c == '']
+            offices = [i for j, i in enumerate(offices) if j not in empty_cells]
+            candidates = [c for c in cands if not c == ''][:len(offices)]
             raw_parties = sheet.row_values(1)[1:-1][:len(offices)]
             parties = []
             for p in raw_parties:
@@ -162,13 +261,27 @@ class WYLoader(WYBaseLoader):
                     cands.append(cand)
             candidates = [c.replace('\n', ' ') for c in cands[1:-1]][:len(offices)]
             parties = parties[1:-1][:len(offices)]
+        else:
+            cands = []
+            parties = []
+            raw_cands = [c for c in sheet.row_values(2) if not c == '']
+            for cand in raw_cands:
+                if " - " in cand:
+                    parties.append(cand.split(' - ')[1].strip())
+                    cands.append(cand.split(' - ')[0].strip())
+                else:
+                    parties.append(party)
+                    cands.append(cand)
+            candidates = [c.replace('\n', ' ') for c in cands[:len(offices)]]
+            if parties[0] == '':
+                parties = parties[1:]
         return zip(candidates, offices, parties)
 
     def _build_contest_kwargs(self, office):
         # find a district number, if one exists (state house & senate only)
         if any(c.isdigit() for c in office):
-            office = 'State ' + office.split(' ')[0]
             district = ''.join([c for c in office if c.isdigit()])
+            office = 'State ' + office.split(' ')[0]
         else:
             district = None
         kwargs = {
@@ -199,7 +312,6 @@ class WYLoader(WYBaseLoader):
 
     def _prep_precinct_result(self, precinct, candidate, office, party, votes):
         # each precinct has multiple candidate totals, plus write-ins, over and under votes
-        print candidate
         kwargs = self._base_kwargs(candidate, office, party)
         if party:
             kwargs.update({'primary_party': party})
@@ -239,7 +351,7 @@ class WYLoader(WYBaseLoader):
 
 class WYLoaderCSV(WYBaseLoader):
     """
-    Loads Wyoming results for 2006 and for special elections.
+    Loads Wyoming results for 2006 and for 2002 special election.
 
     Format:
 
@@ -278,6 +390,66 @@ class WYLoaderCSV(WYBaseLoader):
                     rr_kwargs.update({
                         'party': row['party'].strip(),
                         'jurisdiction': row['precinct'].strip(),
+                        'votes': int(row['votes'].strip()),
+                        'winner': row['winner'].strip(),
+                        'county_ocd_id': self.mapping['ocd_id'],
+                    })
+                    results.append(RawResult(**rr_kwargs))
+        RawResult.objects.insert(results)
+
+    def _skip_row(self, row):
+        return row['office'].strip() not in self.target_offices
+
+    def _build_contest_kwargs(self, row):
+        return {
+            'office': row['office'].strip(),
+            'district': row['district'].strip(),
+        }
+
+    def _build_candidate_kwargs(self, row):
+        return {
+            'full_name': row['candidate'].strip()
+        }
+
+class WYSpecialLoader2008(WYBaseLoader):
+    """
+    Loads Wyoming results for 2008 special elections.
+
+    Format:
+
+    Wyoming has PDF files that have been converted to CSV files with office names that correspond
+    to those used for elections in 2006 and for special elections.
+    """
+
+    def load(self):
+        headers = [
+            'candidate',
+            'office',
+            'district',
+            'party',
+            'county',
+            'votes',
+            'winner'
+        ]
+        self._common_kwargs = self._build_common_election_kwargs()
+        self._common_kwargs['reporting_level'] = 'county'
+        # Store result instances for bulk loading
+        results = []
+
+        with self._file_handle as csvfile:
+            reader = unicodecsv.DictReader(csvfile, fieldnames=headers, encoding='latin-1')
+            for row in reader:
+                if row['votes'] == 'votes':
+                    continue
+                if row['county'].strip() == '':
+                    total_votes = int(row['votes'].strip())
+                else:
+                    rr_kwargs = self._common_kwargs.copy()
+                    rr_kwargs.update(self._build_contest_kwargs(row))
+                    rr_kwargs.update(self._build_candidate_kwargs(row))
+                    rr_kwargs.update({
+                        'party': row['party'].strip(),
+                        'jurisdiction': row['county'].strip(),
                         'votes': int(row['votes'].strip()),
                         'winner': row['winner'].strip(),
                         'county_ocd_id': self.mapping['ocd_id'],
