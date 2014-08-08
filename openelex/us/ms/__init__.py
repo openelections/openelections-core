7bedacede6bc8063bd1e58d567438077e7ad3f38
Derek Willis <dwillis@gmail.com>
2014-08-08 07:07:46 -0400

    added initial ms datasource and mappings

---
+++ openelex/us/ms/__init__.py
@@ -0,0 +1,12 @@
+import sys
+
+# This is a pythonpath hack to avoid shadowing third-party 
+# module with our openelex.us directory
+from openelex import PROJECT_ROOT
+try:
+    sys.path.remove(PROJECT_ROOT)
+except ValueError:
+    pass
+
+from us import STATES
+STATE_POSTALS = map(lambda state: state.abbr, STATES)
---
+++ openelex/us/ms/datasource.py
@@ -0,0 +1,115 @@
+"""
+Mississippi has CSV files containing precinct-level results for each county and all offices 
+for all years back to 2003. All of the files are pre-processed and available on Github at 
+https://github.com/openelections/openelections-data-ms.
+"""
+from os.path import join
+import json
+import datetime
+import urlparse
+
+from openelex import PROJECT_ROOT
+from openelex.base.datasource import BaseDatasource
+from openelex.lib import build_github_url
+
+class Datasource(BaseDatasource):
+    
+    # PUBLIC INTERFACE
+    def mappings(self, year=None):
+        """Return array of dicts containing source url and 
+        standardized filename for raw results file, along 
+        with other pieces of metadata
+        """
+        mappings = []
+        for yr, elecs in self.elections(year).items():
+            mappings.extend(self._build_metadata(yr, elecs))
+        return mappings
+
+    def target_urls(self, year=None):
+        "Get list of source data urls, optionally filtered by year"
+        return [item['raw_url'] for item in self.mappings(year)]
+
+    def filename_url_pairs(self, year=None):
+        return [(item['generated_filename'], item['raw_url']) 
+                for item in self.mappings(year)]
+
+    def mappings_for_url(self, url):
+        return [mapping for mapping in self.mappings() if mapping['raw_url'] == url]
+
+    # PRIVATE METHODS
+
+    def _build_metadata(self, year, elections):
+        meta = []
+        year_int = int(year)
+        for election in elections:
+            if 'special' in election['slug']:
+                results = [x for x in self._url_paths() if x['date'] == election['start_date']]
+                for result in results:
+                    generated_filename = self._generate_special_filename(election, result)
+                    meta.append({
+                        "generated_filename": generated_filename,
+                        "raw_url": self._build_raw_url(year, result['url']),
+                        "pre_processed_url": build_github_url(self.state, generated_filename),
+                        "ocd_id": 'ocd-division/country:us/state:ms',
+                        "name": 'Mississippi',
+                        "election": election['slug']
+                    })
+            else:
+                results = [x for x in self._url_paths() if x['date'] == election['start_date']]
+                for result in results:
+                    generated_filename = self._generate_county_filename(election, result)
+                    meta.append({
+                        "generated_filename": generated_filename,
+                        "raw_url": self._build_raw_url(year, result['url']),
+                        "pre_processed_url": build_github_url(self.state, generated_filename),
+                        "ocd_id": ,
+                        "name": result['county'],
+                        "election": election['slug']
+                    })
+        return meta
+
+    def _generate_special_filename(self, url, start_date, result):
+        bits = [
+            start_date.replace('-',''),
+            self.state,
+        ]
+        if result['party']:
+            bits.append(result['party'].lower())
+        bits.extend([
+            'special',
+            result['race_type'].lower(),
+            result['county'].replace(' ','_').lower()
+        ])
+        bits.append(result['office'])
+        if result['district']:
+            bits.append(result['district'])
+        bits.append('precinct')
+        filename = "__".join(bits) + '.csv'
+        return filename
+
+    def _generate_county_filename(self, url, start_date, result):
+        bits = [
+            start_date.replace('-',''),
+            self.state,
+        ]
+        if result['party']:
+            bits.append(result['party'].lower())
+        bits.extend([
+            result['race_type'].lower(),
+            result['county'].replace(' ','_').lower()
+        ])
+        bits.append('precinct')
+        filename = "__".join(bits) + '.csv'
+        return filename
+
+    def _jurisdictions(self):
+        """Mississippi counties"""
+        m = self.jurisdiction_mappings()
+        mappings = [x for x in m if x['county'] != ""]
+        return mappings
+
+    def _url_for_fetch(self, mapping):
+        if mapping['pre_processed_url']:
+            return mapping['pre_processed_url']
+        else:
+            return mapping['raw_url']
