a9ffd515939b2096ef59fcd3b5d50f72a0c8390a
Geoff Hing <geoffhing@gmail.com>
2014-10-24 23:02:11 -0500

    Add dependencies to setup.py
    Add dependencies to setup.py using the ``install_requires``
    argument to ``setup()``.
    
    Pare down the list of dependencies
    from the one in requirements.txt to only include the top-level
    dependencies that are directly imported in the openelex package.
    It looks like the original list in requirements.txt was created
    by someone just running pip freeze.
    
    Addresses https://github.com/openelections/core/issues/217

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # TODO: More informative message about Python version support, version reflected
 # classifiers argument to setup(). Perhaps setup should fail altogether for 
@@ -16,7 +16,28 @@ setup(
     author='OpenElections',
     author_email='openelections@gmail.com',
     url='http://openelections.net',
-    packages=['openelex'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'beautifulsoup4==4.3.2',
+        'blinker==1.3',
+        'boto==2.9.7',
+        'ipython==0.13.2',
+        'mongoengine==0.8.4',
+        'nameparser==0.2.7',
+        'ordered-set==1.1',
+        'requests>=2.0',
+        'unicodecsv==0.9.4',
+        'us==0.7',
+        'xlrd==0.9.2',
+        'github3.py==0.9.0',
+        'click==3.3',
+    ],
+    tests_require=[
+        'mock==1.0.1',
+        'nose==1.3.0',
+        'factory_boy>=2.3',
+    ],
     entry_points={
         'console_scripts': [
             'openelex = openelex.tasks:cli',
