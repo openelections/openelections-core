b06aa6bb13ab2b46144093fb51f8149980e7b6c2
Eric <ericscottlagergren@gmail.com>
2014-11-02 08:52:44 -0800

    typo

--- openelex/tasks/utils.py
+++ openelex/tasks/utils.py
@@ -14,7 +14,7 @@ def load_module(state, mod_list=[]):
 def default_state_options(f):
     """Decorator that adds the default options for a state command"""
     d1 = click.option('--state', '-s', help='Two-letter state-abbreviation, e.g. NY')
-    d2 = click.option('--datefilter', '-d', help='A year specified as YYYY, e.g. 2012', datefilter='')
+    d2 = click.option('--datefilter', '-d', help='A year specified as YYYY, e.g. 2012', default='')
     return d1(d2(f))
 
 
