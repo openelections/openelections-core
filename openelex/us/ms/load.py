25657fc6986c6e15bb5635d0001db5ecb7874c25
Derek Willis <dwillis@nytimes.com>
2014-12-06 18:08:41 -0500

    fixed MS loader for specials

--- openelex/us/ms/load.py
+++ openelex/us/ms/load.py
@@ -21,9 +21,13 @@ class LoadResults(object):
     """
 
     def run(self, mapping):
-        election_id = mapping['election']
-        if any(s in election_id for s in ['2011', '2012', '2013', '2014']):
-            loader = MSCSVLoader()
+        election_id = mapping['pre_processed_url']
+        if 'precinct' in election_id:
+            loader = MSPrecinctLoader()
+        elif 'special' in election_id:
+            loader = MSSpecialLoader()
+        else:
+            raise "loader not implemented"
         loader.run(mapping)
 
 
@@ -59,7 +63,7 @@ class MSBaseLoader(BaseLoader):
         """
         return False
 
-class MSCSVLoader(MSBaseLoader):
+class MSPrecinctLoader(MSBaseLoader):
     """
     Loads Mississippi results for 2003-2014.
 
@@ -98,9 +102,72 @@ class MSCSVLoader(MSBaseLoader):
                     rr_kwargs['primary_party'] = row['party'].strip()
                     rr_kwargs.update(self._build_contest_kwargs(row))
                     rr_kwargs.update(self._build_candidate_kwargs(row))
-                    jurisdiction = row['county'].strip()
+                    jurisdiction = row['precinct'].strip()
+                    county_ocd_id = [c for c in self.datasource._jurisdictions() if c['county'].upper() == row['county'].upper()][0]['ocd_id']
                     rr_kwargs.update({
                         'party': row['party'].strip(),
+                        'jurisdiction': jurisdiction,
+                        'ocd_id': "{}/precinct:{}".format(county_ocd_id, ocd_type_id(jurisdiction)),
+                        'office': row['office'].strip(),
+                        'district': row['district'].strip(),
+                        'votes': int(row['votes'].strip())
+                    })
+                    results.append(RawResult(**rr_kwargs))
+        RawResult.objects.insert(results)
+
+    def _skip_row(self, row):
+        return row['office'].strip() not in self.target_offices
+
+    def _build_contest_kwargs(self, row):
+        return {
+            'office': row['office'].strip(),
+            'district': row['district'].strip(),
+        }
+
+    def _build_candidate_kwargs(self, row):
+        return {
+            'full_name': row['candidate'].strip()
+        }
+
+class MSSpecialLoader(MSBaseLoader):
+    """
+    Loads Mississippi county-level results for 2003-2013 special elections.
+
+    Format:
+
+    Mississippi has PDF files that have been converted to CSV files for elections after 2002.
+    Header rows are identical except that precinct and party are not present. In special elections,
+    candidates run without a party label.
+    """
+
+    def load(self):
+        headers = [
+            'candidate',
+            'office',
+            'district',
+            'county',
+            'votes',
+            'winner'
+        ]
+        self._common_kwargs = self._build_common_election_kwargs()
+        self._common_kwargs['reporting_level'] = 'county'
+        # Store result instances for bulk loading
+        results = []
+
+        with self._file_handle as csvfile:
+            reader = unicodecsv.DictReader(csvfile, fieldnames = headers, encoding='latin-1')
+            for row in reader:
+                if self._skip_row(row):
+                    continue
+                if row['county'].strip() == '':
+                    total_votes = int(row['votes'].strip())
+                    contest_winner = row['winner'].strip()
+                else:
+                    rr_kwargs = self._common_kwargs.copy()
+                    rr_kwargs.update(self._build_contest_kwargs(row))
+                    rr_kwargs.update(self._build_candidate_kwargs(row))
+                    jurisdiction = row['county'].strip()
+                    rr_kwargs.update({
                         'jurisdiction': jurisdiction,
                         'ocd_id': "{}/county:{}".format(self.mapping['ocd_id'],
                             ocd_type_id(jurisdiction)),
