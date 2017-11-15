8f719f7259c6b8853f6ca35e7f3c0d912bc0bc59
Nick Davies <nbdavies@gmail.com>
2017-11-14 19:49:20 -0600

    Stub out WI logic based on OR, MS

---
+++ openelex/us/wi/datasource.py
@@ -0,0 +1,92 @@
+"""
+Wisconsin has Excel files containing precinct(ward)-level results for elections back to 2002.
+All of the files are pre-processed into CSV and available on GitHub at
+https://github.com/openelections/openelections-data-wi.
+
+There is one file per election, including national and state offices.
+The CSV files are named according to whether they're general/primary, and whether they're special.
+"""
+from future import standard_library
+standard_library.install_aliases()
+from os.path import join
+import json
+import datetime
+import urllib.parse
+
+from openelex import PROJECT_ROOT
+from openelex.base.datasource import BaseDatasource
+from openelex.lib import build_raw_github_url
+
+class Datasource(BaseDatasource):
+
+		# PUBLIC INTERFACE
+		def mappings(self, year=None):
+				"""
+				Return array of dicts containing source url and
+				standardized filename for raw results file, along
+				with other pieces of metadata
+				"""
+				mappings = []
+				for yr, elecs in list(self.elections(year).items()):
+						mappings.extend(self._build_metadata(yr, elecs))
+				return mappings
+
+		def target_urls(self, year=None):
+				"Get list of source data urls, optionally filtered by year"
+				return [item['raw_url'] for item in self.mappings(year)]
+
+		def filename_url_pairs(self, year=None):
+				return [(mapping['generated_filename'], self._url_for_fetch(mapping))
+								for mapping in self.mappings(year)]
+
+		def _url_for_fetch(self, mapping):
+				try:
+						return mapping['pre_processed_url']
+				except KeyError:
+						return mapping['raw_url']
+
+		def mappings_for_url(self, url):
+				return [mapping for mapping in self.mappings() if mapping['raw_url'] == url]
+
+		# PRIVATE METHODS
+
+		def _build_metadata(self, year, elections):
+			  meta = []
+			  year_int = int(year)
+			  for election in elections:
+			  	  results = [x for x in self._url_paths() if x['date'] == election['start_date'] and x['special'] == election['special']]
+			  	  for result in results:
+			  	  	  if result['url']:
+			  	  	  	  raw_url = result['url']
+			  	  	  else:
+			  	  	  	  raw_url = None
+	  	  	  	  generated_filename = self._generate_filename(election['start_date'], election['race_type'], result)
+	  	  	  	  ocd_id = 'ocd-division/country:us/state:wi'
+	  	  	  	  name = "Wisconsin"
+	  	  	  	  meta.append({
+	  	  	  	  	  "generated_filename": generated_filename,
+	  	  	  	  	  "raw_url": raw_url,
+	  	  	  	  	  "pre_processed_url": build_raw_github_url(self.state, election['start_date'][0:4], generated_filename),
+	  	  	  	  	  "ocd_id": ocd_id,
+	  	  	  	  	  "name": name,
+	  	  	  	  	  "election": election['slug']
+	  	  	  	  })
+	  	  return meta
+
+	  def _generate_filename(self, start_date, election_type, result):
+	  	  if result['special']:
+	  	  	  election_type = 'special__' + election_type
+	  	  bits = [
+            start_date.replace('-',''),
+            self.state.lower(),
+            election_type,
+            'ward'
+        ]
+        return "__".join(bits) + '.csv'
+
+		def _jurisdictions(self):
+			  """Wisconsin counties"""
+			  m = self.jurisdiction_mappings()
+			  mappings = [x for x in m if x['county'] != ""]
+			  return mappings
+
---
+++ openelex/us/wi/load.py
@@ -0,0 +1,83 @@
+from builtins import next
+from builtins import object
+import re
+import csv
+import unicodecsv
+
+from openelex.base.load import BaseLoader
+from openelex.models import RawResult
+from openelex.lib.text import ocd_type_id, slugify
+from .datasource import Datasource
+
+"""
+Wisconsin elections have pre-processed CSV results files for elections beginning in 2002.
+These files contain ward-level results. The CSV versions of those are contained in the
+https://github.com/openelections/openelections-data-wi repository.
+"""
+
+class LoadResults(object):
+	  """
+	  Entry point for data loading.
+	  Determines appropriate loader for file and triggers load process.
+	  """
+
+	  def run(self, mapping):
+	  	  election_id = mapping['generated_filename']
+	  	  if 'precinct' in election_id:
+	  	  	  loader = ORPrecinctLoader()
+	  	  else:
+	  	  	  loader = ORLoader()
+	  	  loader.run(mapping)
+
+class WIBaseLoader(BaseLoader):
+	  datasource = Datasource()
+
+class WIPrecinctLoader(WIBaseLoader):
+    """
+    Loads Wisconsin precinct results for 2002-2017.
+
+    Format:
+    Wisconsin has Excel files that have been pre-processed to CSV for elections after	2002.
+    """
+
+    def load(self):
+        self._common_kwargs = self._build_common_election_kwargs()
+        self._common_kwargs['reporting_level'] = 'precinct'
+        # Store result instances for bulk loading
+        results = []
+
+        with self._file_handle as csvfile:
+            reader = unicodecsv.DictReader(csvfile)
+            next(reader, None)
+            for row in reader:
+                rr_kwargs = self._common_kwargs.copy()
+                rr_kwargs.update(self._build_contest_kwargs(row))
+                rr_kwargs.update(self._build_candidate_kwargs(row))
+                rr_kwargs.update(self._build_jurisdiction_kwargs(row))
+                rr_kwargs.update({
+                		'primary_party': row['party'].strip()
+                    'party': row['party'].strip(),
+                    'votes': int(float(row['votes']))
+                })
+                results.append(RawResult(**rr_kwargs))
+        RawResult.objects.insert(results)
+
+    def _build_jurisdiction_kwargs(self, row):
+    		jurisdiction = row['ward'].strip()
+    		county_ocd_id = [c for c in self.datasource._jurisdictions() if c['county'].strip().upper() == row['county'].strip().upper()][0]['ocd_id']       
+    	  return {
+		        'jurisdiction': jurisdiction,
+            'parent_jurisdiction': row['county'],
+            'ocd_id': "{}/precinct:{}".format(county_ocd_id, ocd_type_id(jurisdiction)),
+    	  }
+
+    def _build_contest_kwargs(self, row):
+        return {
+            'office': row['office'].strip(),
+            'district': row['district'].strip(),
+        }
+
+    def _build_candidate_kwargs(self, row):
+        return {
+            'full_name': row['candidate'].strip()
+        }
